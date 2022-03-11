dbfd4210dd597b6ff9bac2e19a956e635ca03715
SEONJAEK <nalalasjsj@gmail.com>
2022-03-11 09:41:13 +0900

    basic

---
+++ 1.basics/ex08/src/04/Content.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function content() {
+  return (
+   // <div>content</div>
+   React.createElement('p', null, 'JSX Tutorials- 특징4: Pure React(React API)로 컴포넌트 작성하기')
+  )
+}
+
+export default content;
